64ae11930425de061396168671ed882d5ab1cf0c
alcai3 <alcai3@697-334-Forlond.sf.k12.internal>
2014-11-05 11:30:16 -0800

    Added first name to index.html

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip Bubba;//your variable declarations here
 public void setup() 
 {
@@ -14,7 +30,7 @@ public void draw()
 }
 public void keyPressed()
   {
-    float accelSpeed = .3;
+    float accelSpeed = .3f;
     if (keyPressed && keyCode == LEFT){
       Bubba.myPointDirection -= 20;
     } else if (keyPressed && keyCode == RIGHT){
@@ -148,3 +164,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
     
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
